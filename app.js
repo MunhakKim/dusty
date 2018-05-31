163436964fb78510abc46d3411311006763a0cbb
조형일 <harryjo1991@gmail.com>
2018-05-31 13:09:13 +0900

    api test

--- app.js
+++ app.js
@@ -51,4 +51,38 @@ app.post('/message',function(req,res) {
             break;
     }
 });
-http.createServer(app).listen(9090, function() { console.log('서버 실행 중...\n')});
+
+//var urlencode = require('urlencode'); console.log(urlencode('변환')); //console.log(urlencode.decode('%EB%B3%80%ED%99%98'));
+
+//http.createServer(app).listen(9090, function() { console.log('서버 실행 중...\n')});
+
+//http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=%EC%84%9C%EC%B4%88%EA%B5%AC&dataTerm=daily&pageNo=1&numOfRows=1&ServiceKey=IRw86Lln5mLdFBdurIth3h45Kk3ubLoozSjOupqmmHlqfN8Ozk3ztxyL2lTxHZfMiikCtHrooV0K56UyBW3JBw%3D%3D&ver=1.3
+
+var station_name ='서초구';
+var urlencode = require('urlencode');
+var encoded_stname  = urlencode(station_name);
+console.log(encoded_stname);
+
+const $url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?';
+const $KEY = 'IRw86Lln5mLdFBdurIth3h45Kk3ubLoozSjOupqmmHlqfN8Ozk3ztxyL2lTxHZfMiikCtHrooV0K56UyBW3JBw%3D%3D&ver=1.3';
+const $station = encoded_stname;
+var $api_url = $url + 'ServiceKey=' + $KEY + '&stationName=' + $station +'&dataTerm=daily&pageNo=1&numOfRows=1&ver=1.3'
+
+
+var url_api_ans;
+let request=require('request');
+let cheerio = require('cheerio');
+request($api_url,function(error,response,body){
+    $ = cheerio.load(body);
+    $('item').each(function(idx) {
+        let pm10 = $(this).find('pm10Value').text();
+        let pm25 = $(this).find('pm25Value').text();
+        console.log(pm10);
+        console.log(pm25);
+    });
+});
+/*request($api_url,function(error,response,body){
+    if(!error&&response.statusCode==200)
+  console.log(body);
+});*/
+
