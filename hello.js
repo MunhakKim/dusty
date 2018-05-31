7f14d6d1a742e3786a71e39d6e2e772ca40ef8c9
김문학 <MHKim@gimmunhag-ui-MacBook.local>
2018-05-31 11:56:30 +0900

    urltest

--- hello.js
+++ hello.js
@@ -1,5 +1,14 @@
+var urlencode = require('urlencode');
+var stationNameTest = urlencode('종로구');
+
+console.log(stationNameTest);
+
 var request=require('request');
-request('http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=%EC%84%9C%EC%B4%88%EA%B5%AC&dataTerm=daily&pageNo=1&numOfRows=1&ServiceKey=IRw86Lln5mLdFBdurIth3h45Kk3ubLoozSjOupqmmHlqfN8Ozk3ztxyL2lTxHZfMiikCtHrooV0K56UyBW3JBw%3D%3D&ver=1.3',function(error, response, body){
+var urlTest='http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName='+stationNameTest+'&dataTerm=daily&pageNo=1&numOfRows=1&ServiceKey=IRw86Lln5mLdFBdurIth3h45Kk3ubLoozSjOupqmmHlqfN8Ozk3ztxyL2lTxHZfMiikCtHrooV0K56UyBW3JBw%3D%3D&ver=1.3';
+
+console.log(urlTest);
+
+request(urlTest,function(error, response, body){
 if(!error&&response.statusCode==200)
   console.log(body);
 });
